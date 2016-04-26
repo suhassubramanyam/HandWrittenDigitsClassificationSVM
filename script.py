ed607771a34c077c9a92d7dac58705b9458b46be
suhas subramanyam <suhassub@buffalo.edu>
2016-04-25 23:31:37 -0400

    Merging changes

--- script.py
+++ script.py
@@ -268,7 +268,7 @@ for i in range(n_class):
 # Logistic Regression with Gradient Descent
 W = np.zeros((n_feature + 1, n_class))
 initialWeights = np.zeros((n_feature + 1, 1))
-opts = {'maxiter': 1}
+opts = {'maxiter': 100}
 for i in range(n_class):
     labeli = Y[:, i].reshape(n_train, 1)
     args = (train_data, labeli)
